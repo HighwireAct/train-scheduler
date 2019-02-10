390edc85f4bd6d0f6036e92942e0b2d9d0749c8d
Ethan Smoller <ethansmoller@gmail.com>
2019-02-09 16:21:15 -0800

    Consolidated config info to app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-import config from "./config.js";
-
-console.dir(config);
+// Firebase config info
+const config = {
+  apiKey: "AIzaSyAR3rfWjtU_wUvtLgT-9_ag6gLQSZ6ITME",
+  authDomain: "train-scheduler-169a0.firebaseapp.com",
+  databaseURL: "https://train-scheduler-169a0.firebaseio.com",
+  projectId: "train-scheduler-169a0",
+  storageBucket: "",
+  messagingSenderId: "873279498479"
+};
 
 // Initialize Firebase
 firebase.initializeApp(config);
