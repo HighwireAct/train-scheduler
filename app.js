4a9bbd7cff8a08ee4d01ac6fb48fb06428ecda8b
Ethan Smoller <ethansmoller@gmail.com>
2019-02-09 18:29:54 -0800

    Got rid of config.js again!

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-import config from "./config.js";
+// Firebase config info
+const config = {
+  apiKey: "AIzaSyAR3rfWjtU_wUvtLgT-9_ag6gLQSZ6ITME",
+  authDomain: "train-scheduler-169a0.firebaseapp.com",
+  databaseURL: "https://train-scheduler-169a0.firebaseio.com",
+  projectId: "train-scheduler-169a0",
+  storageBucket: "",
+  messagingSenderId: "873279498479"
+};
 
 // Initialize Firebase
 firebase.initializeApp(config);
