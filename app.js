3aed220b539c27fa2e11c5d241926a06e99d3ff3
Ethan Smoller <ethansmoller@gmail.com>
2019-02-09 18:15:29 -0800

    Re-hid config file

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// Firebase config info
-const config = {
-  apiKey: "AIzaSyAR3rfWjtU_wUvtLgT-9_ag6gLQSZ6ITME",
-  authDomain: "train-scheduler-169a0.firebaseapp.com",
-  databaseURL: "https://train-scheduler-169a0.firebaseio.com",
-  projectId: "train-scheduler-169a0",
-  storageBucket: "",
-  messagingSenderId: "873279498479"
-};
+import config from "./config.js";
 
 // Initialize Firebase
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
+database.ref().on("value", (dataSnapshot) => {
+  console.log(dataSnapshot.val());
+});
