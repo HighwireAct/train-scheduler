b7cdb5e0a24acd5fcb4ae408b90e65ac23f80fdb
Ethan Smoller <ethansmoller@gmail.com>
2019-02-09 16:17:32 -0800

    Removed config from .gitignore

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase config info
+const config = {
+  apiKey: "AIzaSyAR3rfWjtU_wUvtLgT-9_ag6gLQSZ6ITME",
+  authDomain: "train-scheduler-169a0.firebaseapp.com",
+  databaseURL: "https://train-scheduler-169a0.firebaseio.com",
+  projectId: "train-scheduler-169a0",
+  storageBucket: "",
+  messagingSenderId: "873279498479"
+};
+
+export default config;
