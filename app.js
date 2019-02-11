473f4b7e126568bbbc7bb9e070e115d6f1a20d3f
Ethan Smoller <ethansmoller@gmail.com>
2019-02-10 18:04:29 -0800

    Version 1.0

--- app.js
+++ app.js
@@ -13,6 +13,93 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on("value", (dataSnapshot) => {
-  console.log(dataSnapshot.val());
+// Get 
+database.ref().on("child_added", (snapshot) => {
+  const data = snapshot.val();
+
+  // Prepare data for insertion into table
+  const line = data.line;
+  const frequency = data.frequency;
+  const destination = data.destination;
+  const minutesAway = timeToNextTrain(data.initialTrain, Number(frequency));
+  const nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
+
+  addRow(line, frequency, destination, nextArrival, minutesAway);
 });
+
+// Handler for train form submission
+$("#new-train").submit(function(event) {
+  event.preventDefault();
+  
+  // Check whether input fields are blank
+  const inputsEmpty = {
+    line: $("#line").val().trim() === "",
+    destination: $("#destination").val().trim() === "",
+    initialTrain: $("#initial-train").val().trim() === "",
+    frequency: $("#frequency").val().trim() === ""
+  }
+
+  if (inputsEmpty.line || inputsEmpty.destination || inputsEmpty.initialTrain || inputsEmpty.frequency) {
+    alert("All fields must contain text.");
+  } else {
+      // Create temporary object to hold value for submitted train line
+      const newTrain = {
+        line: $("#line").val().trim(),
+        destination: $("#destination").val().trim(),
+        initialTrain: $("#initial-train").val().trim(),
+        frequency: $("#frequency").val().trim()
+      }
+      // Push train info into database
+      database.ref().push(newTrain);
+
+      // Clear input fields
+      $("#line").val("");
+      $("#destination").val("");
+      $("#initial-train").val("");
+  }
+});
+
+/**
+ * Calculates the amount of time (in minutes) until the next train arrives
+ * @param {number} initTime - Initial departure time in HH:mm format 
+ * @param {number} frequency - Arrival frequency in minutes
+ * @returns {number} - Time until next train arrives (minutes)
+ */
+const timeToNextTrain = (initTime, frequency) => {
+  const initTimeMoment = moment(initTime, "HH:mm");
+  console.log(initTimeMoment);
+  const timeSinceFirstTrain = moment().diff(initTimeMoment, "minutes");
+  console.log(timeSinceFirstTrain);
+
+  return frequency - (timeSinceFirstTrain % frequency);
+};
+
+/**
+ * Adds a row of data to the train table
+ * @param {string} line - Train line
+ * @param {string} frequency - Arrival frequency in minutes
+ * @param {string} destination - Train's final destination
+ * @param {string} nextArrival - When the next train will be arriving
+ * @param {string} minutesAway - How many minutes away the next train is
+ */
+const addRow = (line, frequency, destination, nextArrival, minutesAway) => {
+  // Initialize row
+  const $row = $("<tr>");
+  
+  // Populate cell data
+  const $line = $("<td>").text(line);
+  const $frequency = $("<td>").text(frequency);
+  const $destination = $("<td>").text(destination);
+  const $nextArrival = $("<td>").text(nextArrival);
+  const $minutesAway =$("<td>").text(minutesAway);
+
+  // Append data to row
+  $row.append($line);
+  $row.append($destination);
+  $row.append($frequency);
+  $row.append($nextArrival);
+  $row.append($minutesAway);
+
+  // Append row to table
+  $("#train-data").append($row);
+}
